4614cee81d5e3a9cd96b7ad4776e509d32eafc53
Benoît Garreau <onyx_nagarian47@hotmail.fr>
2021-10-29 10:42:42 +0200

    fix: local dev

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://localhost:4400',
+      target: 'http://localhost:4400',
       changeOrigin: true,
       ws: true,
       secure: false,
