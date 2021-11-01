525e63b8aff409f7cde0d06bba0d7e9ad04ff36b
Benoît Garreau <onyx_nagarian47@hotmail.fr>
2021-11-01 11:26:35 +0100

    fix(client): dev socket blocked issue

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,12 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
   app.use(
-    '/api',
-    createProxyMiddleware({
+    createProxyMiddleware('/api', {
       target: 'http://localhost:4400',
-      changeOrigin: true,
       ws: true,
-      secure: false,
     }),
   )
 }
