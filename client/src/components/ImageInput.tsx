93fcce5034f93ab02d0a9d6d603a4286e2d5f6c1
Benoît Garreau <onyx_nagarian47@hotmail.fr>
2022-04-18 16:04:58 +0200

    feat(client): convert image to webp to reduce image sizing

--- client/src/components/ImageInput.tsx
+++ client/src/components/ImageInput.tsx
@@ -17,13 +17,14 @@ export function ImageInput ({
         accept="image/png, image/jpeg"
         onChange={async e => {
           const file = e.target.files?.[0]
-          const image = await toBase64(file)
+          const webp = await convertToWebp(file)
+          const image = await toBase64(webp)
           onChange?.({ target: { value: image } } as any)
         }}
       />
 
       {value && (
-        <Image
+        <ImageB
           src={value as string}
           alt=""
           onClickCapture={e => {
@@ -60,7 +61,36 @@ function toBase64 (file?: File): Promise<string | undefined> {
   })
 }
 
-const Image = styled.img`
+function convertToWebp (file?: File): Promise<File | undefined> {
+  if (!file) {
+    return Promise.resolve(undefined)
+  }
+
+  return new Promise<File | undefined>((resolve, reject) => {
+    const image = new Image()
+    image.onload = () => {
+      const canvas = document.createElement('canvas')
+      canvas.width = image.naturalWidth
+      canvas.height = image.naturalHeight
+      canvas.getContext('2d')!.drawImage(image, 0, 0)
+      canvas.toBlob(blob => {
+        if (!blob) {
+          reject(new Error())
+        }
+
+        resolve(
+          new File([blob!], 'my-new-name.webp', {
+            type: blob!.type,
+          }),
+        )
+      }, 'image/webp')
+    }
+
+    image.src = URL.createObjectURL(file)
+  })
+}
+
+const ImageB = styled.img`
   object-fit: contain;
   width: 100%;
   max-height: 20rem;
