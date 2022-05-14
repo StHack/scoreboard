303797b28112ed908e4e64a49431b403c55b833c
Croussy <clroussy@laposte.net>
2022-05-15 00:51:18 +0200

    Refont login/register page

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -17,34 +17,39 @@ export function Register () {
   } = useRegisterForm()
 
   return (
-    <Box display="grid" placeItems="center" maxWidth="60rem" margin="0 auto">
-      <Box display="flex" flexDirection="column">
-        <Banner mb="4" width="100%" />
+    <Box display='grid' placeItems='center' maxWidth='60rem' margin='0 auto'>
+      <Box display='flex'
+           flexDirection='column'
+           backgroundColor='#FFFFFF'
+           p="5"
+           borderRadius='14px'
+           boxShadow='0px 0px 4px 0px rgba(201, 201, 201, 0.8)'>
+        <Banner mb='4' width='100%' />
 
         <Form {...formProps}>
-          <LabelInput label="Username">
-            <TextInput type="text" {...usernameProps} />
+          <LabelInput label='Username'>
+            <TextInput type='text' {...usernameProps} />
           </LabelInput>
 
-          <LabelInput label="Password">
-            <TextInput type="password" {...passwordProps} />
+          <LabelInput label='Password'>
+            <TextInput type='password' {...passwordProps} />
           </LabelInput>
 
-          <LabelInput label="Team">
-            <TextInput type="text" {...teamProps} />
+          <LabelInput label='Team'>
+            <TextInput type='text' {...teamProps} />
           </LabelInput>
 
           {error && (
-            <Box backgroundColor="red" color="white">
+            <Box backgroundColor='red' color='white'>
               {error}
             </Box>
           )}
 
           <Button
-            type="submit"
-            fontSize="2"
-            placeSelf="center"
-            m="3"
+            type='submit'
+            fontSize='2'
+            placeSelf='center'
+            m='3'
             disabled={isLoading}
           >
             Register
