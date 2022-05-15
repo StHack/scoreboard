19264a6abe04fd06e26af58196bbbf2646feabe9
Croussy <clroussy@laposte.net>
2022-05-15 22:36:27 +0200

    Graphic refont list and buttons

--- client/src/pages/ScoreBoard/components/TeamsScoreBoard.tsx
+++ client/src/pages/ScoreBoard/components/TeamsScoreBoard.tsx
@@ -7,23 +7,24 @@ import { space, SpaceProps } from 'styled-system'
 export type TeamsScoreBoardProps = {
   score: TeamScore[]
 }
+
 export function TeamsScoreBoard ({ score }: TeamsScoreBoardProps) {
   return (
     <Table>
       <thead>
-        <tr>
-          <th>Rank</th>
-          <th>Team</th>
-          <th>Score</th>
-          <th>Breakthrough</th>
-        </tr>
+      <tr>
+        <th>Rank</th>
+        <th>Team</th>
+        <th>Score</th>
+        <th>Breakthrough</th>
+      </tr>
       </thead>
       {score.map(cs => (
         <tr key={cs.team}>
           <td>{cs.rank}</td>
           <td>{cs.team}</td>
           <td>{cs.score}</td>
-          <Box as="td" display="flex">
+          <Box as='td' display='flex'>
             {cs.breakthroughs.map(({ username, createdAt, challenge }, i) => (
               <Box
                 key={i}
@@ -31,7 +32,7 @@ export function TeamsScoreBoard ({ score }: TeamsScoreBoardProps) {
                   'fr',
                 )}`}
               >
-                <Logo size="2" />
+                <Logo size='2' />
               </Box>
             ))}
           </Box>
@@ -43,16 +44,27 @@ export function TeamsScoreBoard ({ score }: TeamsScoreBoardProps) {
 
 const Table = styled.table<SpaceProps>`
   border-collapse: collapse;
-  border: 2px solid rgb(200, 200, 200);
-  ${space}
+  border-radius: 5px;
 
-  td,
-  th {
-    border: 1px solid rgb(190, 190, 190);
+  ${space}
+  thead tr {
+    background-color: transparent !important;
+  }
+
+  thead th:first-child {
+    border-top-left-radius: 8px;
+  }
+
+  thead th:last-child {
+    border-top-right-radius: 8px;
   }
 
   th {
     background-color: rgb(235, 235, 235);
+    height: 30px;
+    line-height: 30px;
+    font-weight: ${p => p.theme.fontWeights[1]};
+    font-size: ${p => p.theme.fontSizes[0]};
   }
 
   td {
