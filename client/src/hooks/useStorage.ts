32ec5b9587a76759eb02bc999e5fe1c38b9b75e4
Benoît Garreau <onyx_nagarian47@hotmail.fr>
2024-03-16 18:45:21 +0100

    feat(client): force user to read rules
    closes: #35

---
+++ client/src/hooks/useStorage.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect, Dispatch, SetStateAction } from 'react'
+
+export function useStorage<T> (
+  key: string = 'search',
+  defaultValue: T,
+  migration?: (value: any) => T,
+): [T, Dispatch<SetStateAction<T>>] {
+  const [value, setValue] = useState<T>(defaultValue)
+
+  useEffect(() => {
+    const json = localStorage.getItem(key)
+    if (!json) {
+      return
+    }
+
+    const parsed = JSON.parse(json)
+
+    if (!parsed) {
+      return
+    }
+
+    if (migration) {
+      const migrated = migration(parsed)
+      localStorage.setItem(key, JSON.stringify(migrated))
+      setValue(migrated)
+    } else {
+      setValue(parsed)
+    }
+  }, [key, migration, setValue])
+
+  useEffect(() => {
+    if (value && value !== defaultValue) {
+      localStorage.setItem(key, JSON.stringify(value))
+    }
+  }, [defaultValue, key, value])
+
+  return [value, setValue]
+}
