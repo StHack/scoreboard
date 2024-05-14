cfb91ab17e6a852827cbc87284007beddb77279b
Benoît Garreau <onyx_nagarian47@hotmail.fr>
2024-05-14 12:24:04 +0200

    feat(client): allow filtering on attemps chart

--- client/src/components/Charts/ChartAttempts.tsx
+++ client/src/components/Charts/ChartAttempts.tsx
@@ -1,16 +1,20 @@
 import { useTheme } from '@emotion/react'
+import { IconChallenge, IconUsers } from 'components/Icon'
 import { useAdmin } from 'hooks/useAdmin'
+import { useState } from 'react'
 import {
   Bar,
   BarChart,
   Brush,
   CartesianGrid,
+  DefaultLegendContent,
   Legend,
   ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
 } from 'recharts'
+import { Payload } from 'recharts/types/component/DefaultLegendContent'
 import { ChartTooltip } from './ChartTooltip'
 
 export function ChartAttemptsPanel() {
@@ -33,18 +37,65 @@ export function ChartAttemptsPanel() {
 
   const theme = useTheme()
 
+  const [inactiveChalls, setInactiveChalls] = useState<string[]>([])
+  const [activeUsers, setActiveUsers] = useState<string[]>([])
+
+  const handleLegendClick = (data: Payload) => {
+    if (!data.id) {
+      return
+    }
+    const setter = data.id.startsWith('challenge-')
+      ? setInactiveChalls
+      : setActiveUsers
+
+    const value = data.value as string
+    setter(prev =>
+      prev.includes(value) ? prev.filter(el => el !== value) : [...prev, value],
+    )
+  }
+
   return (
     <ResponsiveContainer width="100%" height={400}>
-      <BarChart data={chartData}>
+      <BarChart
+        data={chartData.filter(c => !inactiveChalls.includes(c.challenge))}
+        onClick={nextState => {
+          const active = nextState.activeLabel
+          if (active) {
+            setInactiveChalls(prev => [...prev, active])
+          }
+        }}
+      >
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="challenge" />
         <YAxis />
         <Tooltip content={<ChartTooltip />} />
-        <Legend />
+        <Legend
+          payload={chartData
+            .map<Payload>(cd => ({
+              value: cd.challenge,
+              dataKey: cd.challenge,
+              id: `challenge-${cd.challenge}`,
+              legendIcon: <IconChallenge />,
+              inactive: inactiveChalls.includes(cd.challenge),
+              color: theme.colors.secondary,
+            }))
+            .concat(
+              teams.map((t, i) => ({
+                value: t,
+                id: `team-${t}`,
+                dataKey: `teams["${t}"]`,
+                legendIcon: <IconUsers />,
+                inactive: activeUsers.length > 0 && !activeUsers.includes(t),
+                color: theme.colors.charts[i % theme.colors.charts.length],
+              })),
+            )}
+          onClick={data => handleLegendClick(data)}
+        />
         {teams.map((t, i) => (
           <Bar
             key={t}
             dataKey={`teams["${t}"]`}
+            hide={activeUsers.length > 0 && !activeUsers.includes(t)}
             name={t}
             stackId="a"
             fill={theme.colors.charts[i % theme.colors.charts.length]}
