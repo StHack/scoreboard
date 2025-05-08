bc863cf8977134e92105bb431428e234be5c8a2c
Benoît Garreau <onyx_nagarian47@hotmail.fr>
2025-05-08 19:11:41 +0200

    refactor(client): split ChallengeBlock component

---
+++ client/src/pages/Admin/components/ChallengeBlock.tsx
@@ -0,0 +1,125 @@
+import {
+  Attempt,
+  Challenge,
+  ChallengeScore,
+  Message,
+} from '@sthack/scoreboard-common'
+import { Box } from 'components/Box'
+import { Button } from 'components/Button'
+import { ChallDescriptionPopup } from 'components/ChallDescriptionPopup'
+import { ChallengeCard } from 'components/ChallengeCard'
+import { IconBreak, IconEdit, IconRepair } from 'components/Icon'
+import { ProvidePlayer } from 'hooks/usePlayer'
+import { useState } from 'react'
+
+type ChallengeBlockProps = {
+  chall: Challenge
+  score: ChallengeScore
+  messages: Message[]
+  attempts: Attempt[]
+  onBrokeClick: (chall: Challenge) => void
+  onEditClick: (chall: Challenge) => void
+  onRepairClick: (chall: Challenge) => void
+}
+export function ChallengeBlock({
+  chall,
+  score,
+  messages,
+  attempts,
+  onBrokeClick,
+  onEditClick,
+  onRepairClick,
+}: ChallengeBlockProps) {
+  const { author, category, difficulty, isBroken, name } = chall
+  const lastSolve = score.achievements[score.achievements.length - 1]
+  const [showPreview, setShowPreview] = useState<boolean>(false)
+
+  return (
+    <Box
+      bg="background"
+      borderColor="secondary"
+      borderWidth="medium"
+      borderStyle="solid"
+      borderRadius="small"
+      boxShadow="normal"
+      p="1"
+      gap="2"
+      color={isBroken ? 'red' : ''}
+      display="grid"
+      gridTemplateColumns="auto 1fr"
+      gridTemplateRows="auto 1fr"
+      gridTemplateAreas={`
+        'card stats'
+        'card actions'
+      `}
+      role="listitem"
+    >
+      <Box
+        gap="1"
+        gridArea="card"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+      >
+        <ChallengeCard
+          challenge={chall}
+          score={score}
+          onClick={() => setShowPreview(true)}
+          size="6"
+        />
+        <p>
+          {category} - {difficulty}
+        </p>
+        <p>{author}</p>
+      </Box>
+      <Box gridArea="stats">
+        <Box as="h1" fontSize="2">
+          {name}
+        </Box>
+        <p>
+          Attempted <b>{attempts.length}</b> times
+        </p>
+        <p>
+          Solved <b>{score.achievements.length}</b> times
+          {lastSolve && (
+            <span>
+              {` (last by "${lastSolve.username}" from "${lastSolve.teamname}")`}
+            </span>
+          )}
+        </p>
+      </Box>
+
+      <Box
+        gridArea="actions"
+        display="flex"
+        flexWrap="wrap"
+        gap="1"
+        alignItems="start"
+      >
+        {!isBroken && (
+          <Button onClick={() => onBrokeClick(chall)} icon={IconBreak}>
+            Broke
+          </Button>
+        )}
+        {isBroken && (
+          <Button onClick={() => onRepairClick(chall)} icon={IconRepair}>
+            Repair
+          </Button>
+        )}
+        <Button onClick={() => onEditClick(chall)} icon={IconEdit}>
+          Edit
+        </Button>
+      </Box>
+      {showPreview && (
+        <ProvidePlayer>
+          <ChallDescriptionPopup
+            challenge={chall}
+            messages={messages}
+            onClose={() => setShowPreview(false)}
+            score={score}
+          />
+        </ProvidePlayer>
+      )}
+    </Box>
+  )
+}
--- client/src/pages/Admin/components/ChallengePanel.tsx
+++ client/src/pages/Admin/components/ChallengePanel.tsx
@@ -1,21 +1,13 @@
-import {
-  Attempt,
-  Challenge,
-  ChallengeScore,
-  Message,
-} from '@sthack/scoreboard-common'
 import { Box } from 'components/Box'
 import { Button } from 'components/Button'
-import { ChallDescriptionPopup } from 'components/ChallDescriptionPopup'
-import { ChallengeCard } from 'components/ChallengeCard'
 import { ExportJsonButton } from 'components/ExportJsonButton'
-import { IconBreak, IconCreate, IconEdit, IconRepair } from 'components/Icon'
+import { IconCreate } from 'components/Icon'
 import { SearchInput } from 'components/SearchInput'
 import { useAdmin } from 'hooks/useAdmin'
 import { useGame } from 'hooks/useGame'
-import { ProvidePlayer } from 'hooks/usePlayer'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { ChallengeBlock } from './ChallengeBlock'
 
 export function ChallengePanel() {
   const { challenges, attempts, brokeChallenge, repairChallenge } = useAdmin()
@@ -83,115 +75,3 @@ export function ChallengePanel() {
   )
 }
 
-type ChallengeBlockProps = {
-  chall: Challenge
-  score: ChallengeScore
-  messages: Message[]
-  attempts: Attempt[]
-  onBrokeClick: (chall: Challenge) => void
-  onEditClick: (chall: Challenge) => void
-  onRepairClick: (chall: Challenge) => void
-}
-
-function ChallengeBlock({
-  chall,
-  score,
-  messages,
-  attempts,
-  onBrokeClick,
-  onEditClick,
-  onRepairClick,
-}: ChallengeBlockProps) {
-  const { author, category, difficulty, isBroken, name } = chall
-  const lastSolve = score.achievements[score.achievements.length - 1]
-  const [showPreview, setShowPreview] = useState<boolean>(false)
-
-  return (
-    <Box
-      bg="background"
-      borderColor="secondary"
-      borderWidth="medium"
-      borderStyle="solid"
-      borderRadius="small"
-      boxShadow="normal"
-      p="1"
-      gap="2"
-      color={isBroken ? 'red' : ''}
-      display="grid"
-      gridTemplateColumns="auto 1fr"
-      gridTemplateRows="auto 1fr"
-      gridTemplateAreas={`
-        'card stats'
-        'card actions'
-      `}
-      role="listitem"
-    >
-      <Box
-        gap="1"
-        gridArea="card"
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-      >
-        <ChallengeCard
-          challenge={chall}
-          score={score}
-          onClick={() => setShowPreview(true)}
-          size="6"
-        />
-        <p>
-          {category} - {difficulty}
-        </p>
-        <p>{author}</p>
-      </Box>
-      <Box gridArea="stats">
-        <Box as="h1" fontSize="2">
-          {name}
-        </Box>
-        <p>
-          Attempted <b>{attempts.length}</b> times
-        </p>
-        <p>
-          Solved <b>{score.achievements.length}</b> times
-          {lastSolve && (
-            <span>
-              {` (last by "${lastSolve.username}" from "${lastSolve.teamname}")`}
-            </span>
-          )}
-        </p>
-      </Box>
-
-      <Box
-        gridArea="actions"
-        display="flex"
-        flexWrap="wrap"
-        gap="1"
-        alignItems="start"
-      >
-        {!isBroken && (
-          <Button onClick={() => onBrokeClick(chall)} icon={IconBreak}>
-            Broke
-          </Button>
-        )}
-        {isBroken && (
-          <Button onClick={() => onRepairClick(chall)} icon={IconRepair}>
-            Repair
-          </Button>
-        )}
-        <Button onClick={() => onEditClick(chall)} icon={IconEdit}>
-          Edit
-        </Button>
-      </Box>
-      {showPreview && (
-        <ProvidePlayer>
-          <ChallDescriptionPopup
-            challenge={chall}
-            messages={messages}
-            onClose={() => setShowPreview(false)}
-            score={score}
-          />
-        </ProvidePlayer>
-      )}
-    </Box>
-  )
-}
